5bf0842078453fc663c292e1ce047f10109abd92
micka <salomez@et.esiea.fr>
2017-05-05 16:07:28 +0200

    premiers pas

---
+++ app/src/main/java/com/example/user/myteamappli/DashboardActivity.java
@@ -0,0 +1,21 @@
+package com.example.user.myteamappli;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by micka on 5/5/2017.
+ */
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+
+    }
+}
--- app/src/main/java/com/example/user/myteamappli/MainActivity.java
+++ app/src/main/java/com/example/user/myteamappli/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.user.myteamappli;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.EditText;
+import android.view.View;
+
+import android.support.v7.widget.ButtonBarLayout;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // create the button
+        Button bLogin = (Button) findViewById(R.id.id_bLogin);
+        bLogin.setOnClickListener( new View.OnClickListener(){
+            public void onClick(View v){
+                LaunchDashboard();
+            }
+        });
+    }
+
+    private void LaunchDashboard(){
+        Intent dashboardActivity = new Intent(this, DashboardActivity.class);
+        EditText editext = new (EditText)findViewById(R.id.id_eLogin);
+
     }
 }
