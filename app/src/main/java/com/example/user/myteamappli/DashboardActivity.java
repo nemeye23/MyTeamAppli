ea6487cac5f230773136e9cf1713e04e2e2c807a
micka <salomez@et.esiea.fr>
2017-05-05 18:27:41 +0200

    premiers pas

--- app/src/main/java/com/example/user/myteamappli/DashboardActivity.java
+++ app/src/main/java/com/example/user/myteamappli/DashboardActivity.java
@@ -1,21 +1,33 @@
 package com.example.user.myteamappli;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import android.util.Log;
+import android.widget.TextView;
 
 /**
  * Created by micka on 5/5/2017.
  */
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
 
+        Bundle extra = getIntent().getExtras();
+        //Get the user name
+        String msg = "";
+        if(extra != null)
+            msg = getIntent().getStringExtra(MainActivity.ID_USER);
+        else
+            msg = "error";
+
+
+       TextView textview = (TextView) findViewById(R.id.msg);
+
+        textview.setText(msg);
     }
 }
--- app/src/main/java/com/example/user/myteamappli/MainActivity.java
+++ app/src/main/java/com/example/user/myteamappli/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.user.myteamappli;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.view.View;
 
 import android.support.v7.widget.ButtonBarLayout;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String ID_USER = "user";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +24,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // create the button
-        Button bLogin = (Button) findViewById(R.id.id_bLogin);
+        final Button bLogin = (Button) findViewById(R.id.id_bLogin);
         bLogin.setOnClickListener( new View.OnClickListener(){
             public void onClick(View v){
-                LaunchDashboard();
+                //Check if the pwd is correct
+                Intent dashboardActivity = new Intent(MainActivity.this, DashboardActivity.class);
+
+                EditText pwd = (EditText) findViewById(R.id.id_ePwd);
+                EditText user = (EditText) findViewById(R.id.id_eLogin);
+
+                //Get the value of the EditTexts
+                String pwd_val = pwd.getText().toString();
+                String user_val = user.getText().toString();
+
+                CharSequence  msg_err = "Login and Pwd incorrect !" + pwd_val;
+                Toast toast = Toast.makeText(getApplicationContext(), msg_err, Toast.LENGTH_SHORT);
+
+                //Check authentication
+                if(verification(user.toString(), pwd_val))
+                    LaunchDashboard(dashboardActivity, user_val);
+                else
+                    toast.show();
             }
         });
     }
 
-    private void LaunchDashboard(){
-        Intent dashboardActivity = new Intent(this, DashboardActivity.class);
-        EditText editext = new (EditText)findViewById(R.id.id_eLogin);
+    private void LaunchDashboard(Intent intent, String user){
+        intent.putExtra(ID_USER, user);
+        startActivity(intent);
+    }
 
+    private boolean verification(String user, String pwd){
+        if(pwd.equals("1234"))
+            return true;
+        else
+            return false;
     }
 }
